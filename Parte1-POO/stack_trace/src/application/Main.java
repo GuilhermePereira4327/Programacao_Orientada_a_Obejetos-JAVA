ea2240ae7462d89ae559d784be0423916804ad01
Guilherme Pereira Barbosa <guilhermepereira4237@gmail.com>
2026-02-19 16:38:26 -0300

    Adicionando ultima parte da 1ª parte de POO

---
+++ Parte1-POO/stack_trace/src/application/Main.java
@@ -0,0 +1,40 @@
+package application;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		method1();
+
+		System.out.println("End of program");
+	}
+	
+	public static void method1() {
+		System.out.println("-----Method21 START------");
+		method2();
+		System.out.println("-----Method1 END------");
+	}
+	
+	public static void method2() {
+		System.out.println("-----Method2 START------");
+		Scanner sc = new Scanner(System.in);
+		
+		try {
+			String[] vect = sc.nextLine().split(" ");
+			int position = sc.nextInt();
+			System.out.println(vect[position]);
+		} catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("Invalid position!");
+			e.printStackTrace();
+			sc.next();
+		} catch(InputMismatchException e) {
+			System.out.println("Input erro");
+		}
+		
+		System.out.println("-----Method2 END------");
+		sc.close();
+	}
+
+}
